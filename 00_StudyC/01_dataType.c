470b2709393d6603f930b6265b94bcab825c6f4f
zhangqiuqian <zhangqiuqian01@gmail.com>
2023-02-24 11:28:00 +0800

    02-24 init struct

---
+++ 00_StudyC/01_dataType.c
@@ -0,0 +1,23 @@
+//
+// Created by chaney on 2023/2/24.
+//
+#include <stdio.h>
+
+int main(){
+
+    // 整数类型
+    char a = 'a';  // 字符类型，  -128至127 或 0至255
+    unsigned char b = 'b';  // 无符号
+    signed char c = 'c';  // 带符号
+    int d = 10;  // 整形
+    short e = 1;  // 短整型
+    long f = 213;  // 长整型
+
+
+    printf("%c", a);
+
+    return 0;
+}
+
+
+
