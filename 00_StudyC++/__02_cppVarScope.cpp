a8f41c9e559f211212c2f1e4782d96ae72e53344
zhangqiuqian <zhangqiuqian01@gmail.com>
2023-02-24 19:07:14 +0800

    c++变量的作用域, 定义常量

--- 00_StudyC++/__02_cppVarScope.cpp
+++ 00_StudyC++/__02_cppVarScope.cpp
@@ -4,16 +4,26 @@
 #include <iostream>
 using namespace std;
 
+// 定义常量方式一：使用 #define 预处理器。
+#define PI 3.14
+
 // 定义全局变量
 // 全局变量定义在所有函数的外部，通常在程序的头部
 int g;
 
 int main(){
+    // 定义常量方式二：使用 const 关键字。
+    const int CHINA = 86;
+
     // 定义局部变量
     int a, b;
     a = 10; b = 20;
     g = a + b;
     cout << "g = " << g << endl;
+    // PI = 5.77;  error: lvalue required as left operand of assignment
+    cout << "常量PI = " << PI << endl;
+    // CHINA = 15; error: assignment of read-only variable 'CHINA'
+    cout << "中国 +" << CHINA << endl;
 
     return 0;
 }
